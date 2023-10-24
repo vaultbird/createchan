3b2fe450a740e58e1698fae6a5e736ad793304d4
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2023-10-24 16:02:22 +0700

    chore: Createchan init

---
+++ src/Createchan.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.21;
+
+import {CREATE3} from "@solady/utils/CREATE3.sol";
+
+/// @title Createchan
+/// @author sepyke.eth
+/// @notice CREATE3 Factory
+/// @custom:contract security@vaultbird.com
+contract Createchan {
+    /// @notice Get address
+    /// @param deployer Deployer address
+    /// @param salt Deployment salt
+    /// @return deployed Contract address
+    function getDeployed(
+        address deployer,
+        bytes32 salt
+    ) external view returns (address deployed) {
+        salt = keccak256(abi.encodePacked(deployer, salt));
+        deployed = CREATE3.getDeployed(salt);
+    }
+
+    /// @notice Deploy smart contract
+    /// @param salt Deployment salt
+    /// @param creationCode The contract creation code. e.g. type(MyContract).creationCode
+    /// @return deployed Contract address
+    function deploy(
+        bytes32 salt,
+        bytes memory creationCode
+    ) external payable returns (address deployed) {
+        salt = keccak256(abi.encodePacked(msg.sender, salt));
+        deployed = CREATE3.deploy(salt, creationCode, msg.value);
+    }
+}
